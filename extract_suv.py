ecb343ada592df71cd4fcb34c1220eece96f9252
Biom <toto@ici.com>
2024-04-03 12:09:07 +0200

    fonction extract_SUV

--- extract_suv.py
+++ extract_suv.py
@@ -8,7 +8,7 @@ def do_it(fic):
 	with open(fic, "r") as f :
 		lines = f.read()
 
-	m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)", lines, re.M)
+	m = extract_SUV(lines)
 
 	d = {el[0]: float(el[1]) for el in m}
 	print (d)
@@ -18,8 +18,11 @@ def parse_file():
 	parser.add_argument("fichier", help="nom du fichier", type=str)
 	return parser.parse_args()
 
+def extract_SUV(chaine):
+	m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)", chaine, re.M)
+	return m
+
 if __name__ == "__main__":
 	fic = parse_file()
-	print(fic.fichier)
 	do_it(fic.fichier)
 
