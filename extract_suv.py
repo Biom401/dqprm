a5d34bdf85797af98365b05264fac2b2a7e70189
Biom <toto@ici.com>
2024-04-03 11:06:17 +0200

    meilleur design avec ajout __name__

--- extract_suv.py
+++ extract_suv.py
@@ -1,11 +1,24 @@
 import re
+import argparse
+import os
 
 fichier = "20200527152905.861802.ig.tum"
 
-with open(fichier, "r") as f :
-	lines = f.read()
+def do_it(fic):
+	with open(fic, "r") as f :
+		lines = f.read()
 
-m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)", lines, re.M)
+	m = re.findall("(M.*)(?<!RC)SUVValue = (\d+\.\d+)", lines, re.M)
 
-d = {el[0]: float(el[1]) for el in m}
-print (d)
+	d = {el[0]: float(el[1]) for el in m}
+	print (d)
+
+def parse_file():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("fichier", help="nom du fichier", type=str)
+	return parser.parse_args()
+
+if __name__ == "__main__":
+	fic = parse_file()
+	print(fic.fichier)
+	do_it(fic.fichier)
